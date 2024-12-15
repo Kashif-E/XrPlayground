2419d60ac3264a8a12c5ebcfd70bf6718132de48
kashifmehmood <kashismails@gmail.com>
2024-12-15 13:44:07 +0500

    xr sdk model viewer with sceneview

---
+++ app/src/main/java/com/example/xrplayground/MainActivity.kt
@@ -0,0 +1,232 @@
+package com.example.xrplayground
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.xr.compose.platform.LocalHasXrSpatialFeature
+import androidx.xr.compose.platform.LocalSession
+import androidx.xr.compose.platform.LocalSpatialCapabilities
+import androidx.xr.compose.spatial.EdgeOffset
+import androidx.xr.compose.spatial.Orbiter
+import androidx.xr.compose.spatial.OrbiterEdge
+import androidx.xr.compose.spatial.Subspace
+import androidx.xr.compose.subspace.SpatialPanel
+import androidx.xr.compose.subspace.layout.SpatialRoundedCornerShape
+import androidx.xr.compose.subspace.layout.SubspaceModifier
+import androidx.xr.compose.subspace.layout.height
+import androidx.xr.compose.subspace.layout.movable
+import androidx.xr.compose.subspace.layout.resizable
+import androidx.xr.compose.subspace.layout.width
+import com.example.xrplayground.ui.theme.XrPlaygroundTheme
+import io.github.sceneview.Scene
+import io.github.sceneview.animation.Transition.animateRotation
+import io.github.sceneview.math.Position
+import io.github.sceneview.math.Rotation
+import io.github.sceneview.node.ModelNode
+import io.github.sceneview.rememberCameraNode
+import io.github.sceneview.rememberEngine
+import io.github.sceneview.rememberEnvironmentLoader
+import io.github.sceneview.rememberModelLoader
+import io.github.sceneview.rememberNode
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.DurationUnit.MILLISECONDS
+
+class MainActivity : ComponentActivity() {
+
+    @SuppressLint("RestrictedApi")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            XrPlaygroundTheme {
+                val session = LocalSession.current
+                if (LocalSpatialCapabilities.current.isSpatialUiEnabled) {
+                    Subspace {
+                        MySpatialContent(onRequestHomeSpaceMode = { session?.requestHomeSpaceMode() })
+                    }
+                } else {
+                    My2DContent(onRequestFullSpaceMode = { session?.requestFullSpaceMode() })
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("RestrictedApi")
+@Composable
+fun MySpatialContent(onRequestHomeSpaceMode: () -> Unit) {
+    SpatialPanel(SubspaceModifier.width(1280.dp).height(800.dp).resizable().movable()) {
+        Surface {
+            MainContent(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(48.dp)
+            )
+        }
+        Orbiter(
+            position = OrbiterEdge.Top,
+            offset = EdgeOffset.inner(offset = 20.dp),
+            alignment = Alignment.End,
+            shape = SpatialRoundedCornerShape(CornerSize(28.dp))
+        ) {
+            HomeSpaceModeIconButton(
+                onClick = onRequestHomeSpaceMode,
+                modifier = Modifier.size(56.dp)
+            )
+        }
+    }
+}
+
+@SuppressLint("RestrictedApi")
+@Composable
+fun My2DContent(onRequestFullSpaceMode: () -> Unit) {
+    Surface {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            MainContent(modifier = Modifier.padding(48.dp))
+            if (LocalHasXrSpatialFeature.current) {
+                FullSpaceModeIconButton(
+                    onClick = onRequestFullSpaceMode,
+                    modifier = Modifier.padding(32.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MainContent(modifier: Modifier = Modifier) {
+    Scene()
+}
+
+@Composable
+fun FullSpaceModeIconButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    IconButton(onClick = onClick, modifier = modifier) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_full_space_mode_switch),
+            contentDescription = stringResource(R.string.switch_to_full_space_mode)
+        )
+    }
+}
+
+@Composable
+fun HomeSpaceModeIconButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    FilledTonalIconButton(onClick = onClick, modifier = modifier) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_home_space_mode_switch),
+            contentDescription = stringResource(R.string.switch_to_home_space_mode)
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun My2dContentPreview() {
+    XrPlaygroundTheme {
+        My2DContent(onRequestFullSpaceMode = {})
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FullSpaceModeButtonPreview() {
+    XrPlaygroundTheme {
+        FullSpaceModeIconButton(onClick = {})
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun HomeSpaceModeButtonPreview() {
+    XrPlaygroundTheme {
+        HomeSpaceModeIconButton(onClick = {})
+    }
+}
+
+@Composable
+fun Scene() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        val engine = rememberEngine()
+        val modelLoader = rememberModelLoader(engine)
+        val environmentLoader = rememberEnvironmentLoader(engine)
+
+        val cameraNode = rememberCameraNode(engine).apply {
+            position = Position(z = 4.0f)
+        }
+        val centerNode = rememberNode(engine)
+            .addChildNode(cameraNode)
+        val cameraTransition = rememberInfiniteTransition(label = "CameraTransition")
+        val cameraRotation by cameraTransition.animateRotation(
+            initialValue = Rotation(y = 0.0f),
+            targetValue = Rotation(y = 360.0f),
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 7.seconds.toInt(MILLISECONDS))
+            )
+        )
+        val modelNode = ModelNode(
+            modelInstance = modelLoader.createModelInstance(
+                assetFileLocation = "models/damaged_helmet.glb"
+            ),
+            scaleToUnits = 1.0f
+        )
+        Scene(
+            modifier = Modifier.fillMaxSize(),
+            engine = engine,
+            modelLoader = modelLoader,
+            cameraNode = cameraNode,
+            childNodes = listOf(
+                centerNode,
+                modelNode.also { node ->
+                    DisposableEffect(node) {
+                        onDispose {
+                            node.destroy()
+                        }
+                    }
+                }),
+            environment = environmentLoader.createHDREnvironment(
+                assetFileLocation = "environments/sky_2k.hdr"
+            )!!,
+            onFrame = {
+                centerNode.rotation = cameraRotation
+                cameraNode.lookAt(centerNode)
+            }
+        )
+
+    }
+}
